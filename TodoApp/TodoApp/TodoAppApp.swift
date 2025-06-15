4d3ea2809e3f5097521d847bc3983f191918638f
igor <igorfwds@hotmail.com>
2025-06-15 17:19:34 -0300

    Creating MVVM folder structure

---
+++ TodoApp/TodoApp/TodoAppApp.swift
@@ -0,0 +1,19 @@
+//
+//  TodoAppApp.swift
+//  TodoApp
+//
+//  Created by Igor on 15/06/25.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct TodoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+
+    }
+}
---
+++ TodoApp/TodoApp/View/ContentView.swift
@@ -0,0 +1,19 @@
+//
+//  ContentView.swift
+//  TodoApp
+//
+//  Created by Igor on 15/06/25.
+//
+
+import SwiftUI
+import SwiftData
+
+struct ContentView: View {
+    var body: some View {
+        Text("test")
+    }
+}
+
+#Preview {
+    ContentView()
+}
